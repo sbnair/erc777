2870dc906350f1021ca40d8f7ccf1018d0558bfa
root <root@ip-172-31-13-149.us-east-2.compute.internal>
2021-07-23 14:03:07 +0000

    Added changes in ERC 777

--- contract/src/utils/helper_methods.rs
+++ contract/src/utils/helper_methods.rs
@@ -123,8 +123,8 @@ pub fn _mintcheck(_account: AccountHash, _amount: U256, _data: Bytes, _operator_
     	 return "ERC777: mint to the zero address".as_ptr() as *const c_char;
     }
 
-    set_key(&"total_supply",get_key::<U256>("total_supply").saturating_add(_amount));   
-        
+    set_key(&"total_supply",get_key::<U256>(&"total_supply").saturating_add(_amount));   
+    println!("balance : {}",&balance_key(&_account));        
     set_key(&balance_key(&_account),get_key::<U256>(&balance_key(&_account)).saturating_add(_amount));
 
     return "true".as_ptr() as *const c_char;
