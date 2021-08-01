0ce1963cc968e60ecfb9222f2cf65e3666e4e501
Shailesh B Nair <shai567@live.com>
2021-08-01 12:34:05 +0530

    Updated main files

--- contract/src/main.rs
+++ contract/src/main.rs
@@ -184,7 +184,7 @@ pub extern "C" fn is_operator_for() {
     
     let token_holder: AccountHash = runtime::get_named_arg("token_holder");
     
-    let val: bool = get_key::<bool>(&is_operator_for_main(operator, token_holder));
+    let val: bool = get_key::<bool>(&is_operator_for_main(&operator, &token_holder));
     
     ret(val)
 }
